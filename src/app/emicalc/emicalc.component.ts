e96422a122f4047ddf63a0b792be2bd19481f20d
MansiMakwana <mansi.s.makwana@gmail.com>
2021-09-29 14:42:21 +0530

    first commit

---
+++ src/app/emicalc/emicalc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emicalc',
+  templateUrl: './emicalc.component.html',
+  styleUrls: ['./emicalc.component.css']
+})
+export class EmicalcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
